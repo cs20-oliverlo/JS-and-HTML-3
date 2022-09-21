77171590dff7efdae59a331129216b23fa57e902
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-09-21 10:55:03 -0600

    awd

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // JavaScript Code
+
+document.getElementById("btn").addEventListener("click", helloButton);
+
+function helloButton() {
+   alert("Hello World");
+}
+
+document.getElementById("heading").addEventListener("click", helloHeading)
+
+function helloHeading() {
+    alert("Hello World");
+ }
